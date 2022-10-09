20df372ec977c8d488ec0ad2de72f0b22714593f
hassanqari9 <you@example.comhassanqari9@gmail.com>
2022-10-09 20:12:46 +0530

    a

--- 32 Sorting_Bubble.cpp
+++ 32 Sorting_Bubble.cpp
@@ -3,18 +3,23 @@ using namespace std;
 
 void bubbleSort(int arr[], int n)
 {
-    int temp;
-    for(int i = 0; i < n; i++)
+    int temp,flag;
+    for(int i = 0; i < n-1; i++)
     {
-        for (int j = 0; j < n - 1; j++)
+        flag = 0;
+       
+        for (int j = 0; j < n-1-i; j++)
         {
             if(arr[j] > arr[j+1])
             {
                 temp = arr[j];
                 arr[j] = arr[j+1];
                 arr[j+1] = temp;
+                flag = 1;
             }
-        }    
+        }
+        if(flag==0)
+            break;
     }
 }
 int main()
@@ -22,10 +27,10 @@ int main()
     int arr[100], n;
     cout << "Enter the Size of Array : ";
     cin >> n;
-
-    cout << "Enter the Elements of Array : ";
+ 
+    cout << "Enter the Elements of Array : "<<endl;
     for(int i = 0; i < n; i++)
-        cin >> arr[i];
+       cin >> arr[i];
 
     bubbleSort(arr, n);
     for(int i = 0; i < n; i++)
