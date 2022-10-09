c13d2eb310508e017b28d741c69ace03552b0fc7
hassanqari9 <you@example.comhassanqari9@gmail.com>
2022-10-09 10:56:50 +0530

    a

--- 27 Queue_Array.cpp
+++ 27 Queue_Array.cpp
@@ -1,71 +1,76 @@
-// Queue
-// FIFO(First In First Out)
-// This is mainly used in case of resource allocation
-
 #include <iostream>
-#define max 100
 using namespace std;
-void enque(int );
-int deque();
-void view();
-int queue[max];
-int front = -1, rear = -1;
+int queue[10];
+int front = -1, rear = -1;          // Front for Deletion or Dequeue &&&&&&&&&&& Rear for Insertion or Enqueue
 
+void Enqueue(int x)
+{
+    if (rear == 10 - 1)                          // If Queue is already full
+    {
+        cout << "Queue is Full" << endl;
+    }
+    else                                          // Else Elements will be inserted in Queue using Rear index pointer
+    {
+        rear++;
+        queue[rear] = x;
+    }
+}
+
+int Dequeue()
+{ 
+    int x = -1;                             
+    if (rear == front)                           // If Queue is Empty
+    {
+        cout << "Queue is Empty" << endl;
+    }
+    else                                          // Else Elements will be Dequeued or Deleted using Front index pointer
+    {
+        front++;
+        x = queue[front];
+    }
+    return x;                                      // X Will be returned with either an element or as -1, in that case Queue will be empty
+}
+
+void View()
+{
+    int i;                                   
+    if (rear == front)                              // Condition for Empty Queue
+    { 
+        cout << "Queue is Empty" << endl;
+    }
+    else                                             // Else a loop will be intialised from front = 0 upto rear
+    {
+        for (i = front + 1; i <= rear; i++)
+        {
+            cout << queue[i] << " ";
+        }
+    }
+}
 int main()
 {
     int ch, item;
     while (1)
     {
-        cout << "\n1 For Enque, 2 For Deque, 3 For View, 4 For Exit: " << endl;
+        cout << "\n1 for EnQueue\n2 for DeQueue\n3 for View\n4 for Exit\n Enter Your Choice : ";
         cin >> ch;
+
         if (ch == 1)
         {
-            cout << "Enter item to Enque: " << endl;
+            cout << "Enter the Element : ";
             cin >> item;
-            enque(item);
+            Enqueue(item);
         }
         else if (ch == 2)
         {
-            item = deque();
+            item = Dequeue();
             if (item == -1)
-                cout << "Underflow";
+                cout << "UnderFlow" << endl;
             else
                 cout << item;
         }
         else if (ch == 3)
-            view();
-        else
+            View();
+        else if (ch == 4)
             break;
-    }
-}
-
-void enque(int item)
-{
-    if (rear < max)
-
-        queue[++rear] = item;
-    else
-        cout << "OVerlflow" << endl;
-}
-
-int deque()
-{
-    if (front <= rear)
-
-        return queue[front++];
-    else 
-    return -1;
-        
-}
-
-void view()
-{
-    int i;
-    if(front>rear)
-    cout<<"empty"<<endl;
-    else
-    {
-        for(i=front;i<=rear;i++)
-        cout<<queue[i];
     }
 }
