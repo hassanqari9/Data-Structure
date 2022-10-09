a34d2ed37406442d1c5afb292e904ab8fc5eb826
hassanqari9 <you@example.comhassanqari9@gmail.com>
2022-10-09 13:38:03 +0530

    a

--- 33 Sorting_Insertion.cpp
+++ 33 Sorting_Insertion.cpp
@@ -1,37 +1,27 @@
-/ Insertion sort in C++
-
 #include <iostream>
+
 using namespace std;
-
-// Function to print an array
-void printArray(int array[], int size) {
-  for (int i = 0; i < size; i++) {
-    cout << array[i] << " ";
-  }
-  cout << endl;
-}
-
-void insertionSort(int array[], int size) {
-  for (int step = 1; step < size; step++) {
-    int key = array[step];
-    int j = step - 1;
-
-    // Compare key with each element on the left of it until an element smaller than
-    // it is found.
-    // For descending order, change key<array[j] to key>array[j].
-    while (key < array[j] && j >= 0) {
-      array[j + 1] = array[j];
-      --j;
+  
+void InsertionSort(int A[], int n){
+    for (int i=1; i<n; i++){
+        int j = i-1;
+        int x = A[i];
+        while (j>-1 && A[j] > x){
+            A[j+1] = A[j];
+            j--;
+        }
+        A[j+1] = x;
     }
-    array[j + 1] = key;
-  }
 }
-
-// Driver code
+ 
 int main() {
-  int data[] = {9, 5, 1, 4, 3};
-  int size = sizeof(data) / sizeof(data[0]);
-  insertionSort(data, size);
-  cout << "Sorted array in ascending order:\n";
-  printArray(data, size);
+ 
+    int A[] = {19, 17, 15, 13, 11, 9, 7, 5, 3, 1};
+    int n=10;
+    
+    InsertionSort(A,n);
+    for(int i=0;i<10;i++)
+         cout<<A[i];
+ 
+    return 0;
 }
